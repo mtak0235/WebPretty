20156ec28deb6fba96988a3dc90ea468f3f89b36
soyoungee <soyoung2424@gmail.com>
2019-07-13 22:51:14 +0900

    app 변경
    view 폴더와 public 폴더 생성

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+//var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
+var bodyParser = require('body-parser');
 var db = require('./dbconnection');
 var fs = require('fs');
+
 
 var app = express();
 
 //var userRouter = require('./route/auth');
 var boardRouter = require('./route/board');
 
+app.set('port', process.env.PORT || 3000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 //app.use("/auth",userRouter);
 app.use("/board",boardRouter);
-app.use(express.static('image'));
-app.use(express.static('board'));
+//app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + 'public')));
 
-app.set('port', process.env.PORT || 3000);
+app.use(function(req, res) {
+    next(createError(404));
+})
+
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+})
 
 app.get('/', function(req, res) {
     res.writeHead(200, {"Content-Type":"text/html"});
-    fs.readFile(__dirname + "/mainPage/MAIN.html", (err, data) => {
+    fs.readFile(__dirname + "/MAIN.html", (err, data) => {
         if (err) throw (err);
         res.end(data, 'utf-8');
     })
 });
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port' + app.get('port'))
+})
+
+//module.exports = app;
 
 app.get('/user', function(req, res){
     
@@ -34,7 +61,4 @@ app.get('/user', function(req, res){
     });
 });
 
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port' + app.get('port'))
-})
 
